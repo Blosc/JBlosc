7656b505e472df57845e4648ab5a6ef4b24248b1
aalted <andres.alted@gmail.com>
2016-10-30 08:28:53 +0100

    adding new path jblosc

---
+++ jblosc/src/main/java/com/jblosc/IBloscDll.java
@@ -0,0 +1,37 @@
+package com.jblosc;
+import com.sun.jna.Library;
+import com.sun.jna.NativeLong;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.NativeLongByReference;
+import com.sun.jna.ptr.PointerByReference;
+
+public interface IBloscDll extends Library  {
+
+	void blosc_init();
+	int blosc_get_nthreads();
+	int blosc_set_nthreads(int nthreads);
+	String blosc_get_compressor();
+	int blosc_set_compressor(String compname);
+	int blosc_compcode_to_compname(int compcode, PointerByReference compname);
+	int blosc_compname_to_compcode(String compname);
+	String blosc_list_compressors();
+	String blosc_get_version_string();
+	int blosc_compress (int clevel, int doshuffle, NativeLong typesize, 
+			NativeLong nbytes, Pointer src, Pointer dest, NativeLong destsize);
+	int blosc_compress_ctx(int clevel, int doshuffle, NativeLong typesize, NativeLong nbytes, 
+			Pointer src, Pointer dest, NativeLong destsize, String compressor, NativeLong blocksize, 
+			int numinternalthreads);
+	int blosc_decompress(Pointer src, Pointer dest, NativeLong destsize);
+	int blosc_decompress_ctx(Pointer src, Pointer dest, NativeLong destsize, int numinternalthreads);
+	int blosc_getitem(Pointer src, int start, int nitems, Pointer dest);
+	int blosc_get_complib_info(String compname, PointerByReference complib, PointerByReference version);
+	int blosc_free_resources();
+	void blosc_cbuffer_sizes(Pointer cbuffer, NativeLongByReference nbytes, NativeLongByReference cbytes, NativeLongByReference blocksize);
+	void blosc_cbuffer_metainfo(Pointer cbuffer, NativeLongByReference typesize, IntByReference flags);
+	void blosc_cbuffer_versions(Pointer cbuffer, IntByReference version, IntByReference versionlz);
+	Pointer blosc_cbuffer_complib(Pointer cbuffer);
+	int blosc_get_blocksize();
+	void blosc_destroy();
+	
+}
---
+++ jblosc/src/main/java/com/jblosc/examples/BloscWrapperInvoker.java
@@ -0,0 +1,55 @@
+package com.jblosc.examples;
+
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+
+import com.jblosc.BloscWrapper;
+
+public class BloscWrapperInvoker {
+	public static byte[] FloatArray2ByteArray(float[] values){
+        ByteBuffer buffer = ByteBuffer.allocate(4 * values.length);
+
+        for (float value : values){
+            buffer.putFloat(value);
+        }
+
+        return buffer.array();
+    }
+	
+	private static float[] toFloatArray(byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        FloatBuffer fb = buffer.asFloatBuffer();
+        float[] floatArray = new float[fb.limit()];
+        fb.get(floatArray);
+        return floatArray;
+    }	
+
+	private static double[] toDoubleArray(byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        DoubleBuffer db = buffer.asDoubleBuffer();
+        double[] doubleArray = new double[db.limit()];
+        db.get(doubleArray);
+        return doubleArray;
+    }	
+	
+	public static void main(String[] args) {
+		int SIZE = 100 * 100 * 100;
+		double data[] = new double[SIZE];
+		for (int i=0; i<SIZE; i++) {
+			data[i]=i*2;
+		}
+		byte data_out[];
+		BloscWrapper bw = new BloscWrapper();
+		bw.init();
+//		data_out = bw.compress(5, 1, FloatArray2ByteArray(data));
+		data_out = bw.compress(5, 1, data);
+		System.out.println("Items Original " + SIZE + ", Items Compressed " + data_out.length);
+		double[] data_again=bw.decompressToDoubleArray(data_out);
+//		double data_float_again[] = toDoubleArray(data_again);
+		
+		bw.destroy();
+		System.out.println("Items Original " + data_out.length + ", Items Decompressed " + data_again.length);
+		System.out.println("Finished!");
+	}
+}
