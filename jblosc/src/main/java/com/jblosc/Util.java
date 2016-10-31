1f27366f30147dc8de9cfbce03998c0cef624cde
aalted <andres.alted@gmail.com>
2016-10-31 14:50:09 +0100

    Added Util.java

---
+++ jblosc/src/main/java/com/jblosc/Util.java
@@ -0,0 +1,11 @@
+package com.jblosc;
+
+public class Util {
+	public static String getArchPlatform() {
+		String archDataModel = System.getProperty("sun.arch.data.model");
+		if (archDataModel.equals("64")) {
+			archDataModel = "";
+		}
+		return archDataModel;
+	}
+}
