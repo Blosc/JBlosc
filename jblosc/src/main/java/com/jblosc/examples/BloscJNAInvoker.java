f4296f76e7aa7dc41df69d7b393faf49ad29275a
aalted <andres.alted@gmail.com>
2016-11-10 10:32:55 +0100

    Formatted file

--- jblosc/src/main/java/com/jblosc/examples/BloscJNAInvoker.java
+++ jblosc/src/main/java/com/jblosc/examples/BloscJNAInvoker.java
@@ -12,21 +12,21 @@ public class BloscJNAInvoker {
 	public static void main(String[] args) {
 		int SIZE = 100 * 100 * 100 * 10;
 		float data[] = new float[SIZE];
-		for (int i=0; i<SIZE; i++) {
-			data[i]=i*2;
+		for (int i = 0; i < SIZE; i++) {
+			data[i] = i * 2;
 		}
 		long isize = SIZE * 4;
-		long startTime = System.currentTimeMillis();				
-		IBloscDll iBlosc = (IBloscDll)Native.loadLibrary("blosc", IBloscDll.class);
+		long startTime = System.currentTimeMillis();
+		IBloscDll iBlosc = (IBloscDll) Native.loadLibrary("blosc", IBloscDll.class);
 		Memory m = new Memory(isize);
 		m.write(0, data, 0, SIZE);
 		Memory m2 = new Memory(isize);
 		iBlosc.blosc_init();
 		iBlosc.blosc_set_nthreads(2);
 		System.out.println("Threads " + iBlosc.blosc_get_nthreads());
-		int size = iBlosc.blosc_compress(5, 1,new NativeLong(4), new NativeLong(isize), m, m2, new NativeLong(isize));
-		long stopTime = System.currentTimeMillis();		
-		System.out.println("Compress time " + (stopTime - startTime) + " ms");		
+		int size = iBlosc.blosc_compress(5, 1, new NativeLong(4), new NativeLong(isize), m, m2, new NativeLong(isize));
+		long stopTime = System.currentTimeMillis();
+		System.out.println("Compress time " + (stopTime - startTime) + " ms");
 		System.out.println("Size " + size);
 		iBlosc.blosc_set_compressor("lz4");
 		System.out.println(iBlosc.blosc_get_compressor());
