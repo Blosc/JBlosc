fdba15d1728846167c8f324a95617960fd84e74b
Alberto Sabater <alberto3pt@gmail.com>
2017-07-03 10:17:08 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ set -x
 
 if [[ ! -z $TRAVIS_TAG ]]; then
     sed -i "s/1.0.0/$TRAVIS_TAG/g" "pom.xml"
-    cp .travis.settings.xml $HOME/.m2/settings.xml
+    cp ../.travis.settings.xml $HOME/.m2/settings.xml
     mvn deploy
 else
     mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
