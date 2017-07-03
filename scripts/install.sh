e63439832172b6a08364960e9c7db8fdc4eca419
Alberto Sabater <alberto3pt@gmail.com>
2017-07-03 09:53:29 +0200

    Updated travis with scripts

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ ! -z $TRAVIS_TAG ]]; then
+    sed -i \"s/1.0.0/$TRAVIS_TAG/g\" \"jblosc/pom.xml\"
+    cp .travis.settings.xml $HOME/.m2/settings.xml
+    mvn deploy
+else
+    mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+fi
