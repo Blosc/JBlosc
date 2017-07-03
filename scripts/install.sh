8d2c89817ef868a066a84c821be01778df2573db
Alberto Sabater <alberto3pt@gmail.com>
2017-07-03 10:58:15 +0200

    Updated pom.xml version marker

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 if [[ ! -z $TRAVIS_TAG ]]; then
-    sed -i "s/1.0.0/$TRAVIS_TAG/g" "pom.xml"
+    sed -i "s/jblosc_version_x.y.z/$TRAVIS_TAG/g" "pom.xml"
     cp ../.travis.settings.xml $HOME/.m2/settings.xml
     mvn deploy
 else
