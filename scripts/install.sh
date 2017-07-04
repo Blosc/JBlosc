2e30d57084659402c32fdbeb0b3cd22b6c8aa13f
Francesc Alted <faltet@gmail.com>
2017-07-04 09:47:28 +0200

    Placeholder in uppercase

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 if [[ ! -z $TRAVIS_TAG ]]; then
-    sed -i "s/jblosc_version_x.y.z/$TRAVIS_TAG/g" "pom.xml"
+    sed -i "s/JBLOSC_VERSION_X.Y.Z/$TRAVIS_TAG/g" "pom.xml"
     cp ../.travis.settings.xml $HOME/.m2/settings.xml
     mvn deploy
 else
